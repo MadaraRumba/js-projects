5d45c84393d25d5595323cc01ca3c4a8c3c4474f
MadaraRumba <madara.rumba.rumba@gmail.com>
2024-01-27 12:12:41 +0200

    Notes from lectures on arrays and the assignment

--- udemy-js-fundamentals/02-Fundamentals-Part-2/starter/assignments.js
+++ udemy-js-fundamentals/02-Fundamentals-Part-2/starter/assignments.js
@@ -1,28 +1,68 @@
+'use strict';
+/*
 //JavaScript Fundamentals – Part 2
 //Coding Challenge #1
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 //Data 1
-// const avgDolphins = calcAverage (44, 23, 71 );
-// const avgKoalas = calcAverage (65, 54, 49 );
+let scoreDolphins = calcAverage (44, 23, 71 );
+let scoreKoalas = calcAverage (65, 54, 49 );
 
-//Data 2
-// const avgDolphins = calcAverage(85, 54, 41);
-// const avgKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
 
 //Data 3
-const scoreDolphins = calcAverage (10, 10, 10 );
-const scoreKoalas = calcAverage (10, 10, 10 );
+// const scoreDolphins = calcAverage (10, 10, 10 );
+// const scoreKoalas = calcAverage (10, 10, 10 );
 
-const checkWinner = (avgDolphins, avgKoalas) => {
-    if (avgDolphins >= 2 * avgKoalas) {
-        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+const checkWinner = (scoreDolphins, scoreKoalas) => {
+    if (scoreDolphins >= 2 * scoreKoalas) {
+        console.log(`Dolphins win (${scoreDolphins} vs. ${scoreKoalas})`);
+    } else if (scoreKoalas >= 2 * scoreDolphins) {
+        console.log(`Koalas win (${scoreKoalas} vs. ${scoreDolphins})`);
     } else {
-        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+        console.log('No team wins...');
     }
 }
-
-checkWinner(avgDolphins, avgKoalas);
-
+checkWinner(scoreDolphins, scoreKoalas);
 
 
+//Data 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
 
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+*/
+
+
+//Coding Challenge #2
+
+// function calcTip(bill) {
+//     if (bill <= 300 && bill >= 50) {
+//         const tip = 0.15 * bill;
+//         return tip;
+//     } else {
+//         const tip = 0.20 * bill;
+//         return tip;
+//     }
+// }
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const tip = calcTip(100);
+console.log(tip);
+
+console.log(calcTip(480));
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
--- udemy-js-fundamentals/02-Fundamentals-Part-2/starter/script.js
+++ udemy-js-fundamentals/02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
+//FUNCTIONS
 // function logger(){
 //     console.log('My name is Jonas');
 // }
-//
-// logger();
+//  logger();
 //
 // function fruitProcessor(apples, oranges) {
 //     // console.log(apples, oranges);
@@ -16,7 +16,7 @@
 // console.log(appleJuice);
 // // console.log(fruitProcessor(3, 0)); // logging it directly
 //
-// const appleOrangeJuice = fruitProcessor(2, 4);
+// const appleOrangeJuice = fruitProces sor(2, 4);
 // console.log(appleOrangeJuice);
 
 // FUNCTION DECLARATION
@@ -57,12 +57,13 @@
 // function fruitProcessor(apples, oranges) {
 //     const applePieces= cutFruitPieces(apples);
 //     const orangePieces = cutFruitPieces(oranges);
-//
 //     const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
 //     return juice;
 // };
 // console.log(fruitProcessor(2, 3));
 
+/*
+//FUNCTIONS REVIEW
 const calcAge = function (birthYear){
     return 2037 - birthYear;
 }
@@ -83,3 +84,72 @@ const yearsUntilRetirement = function (birthYear, firstName){
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1950, 'Mike'));
+*/
+
+
+/////////////////////////////////////// ARRAYS
+/*
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array (1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Doe', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//Exercise
+const calcAge = function (birthYear){
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+// console.log(calcAge(years));
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+*/
+/*
+//ARRAY METHODS
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// add
+const newLenght = friends.push('Jay');
+
+console.log(friends);
+console.log(newLenght);
+
+friends.unshift('John');
+console.log(friends);
+
+//removes
+friends.pop();
+const popped = friends.pop();
+console.log(friends);
+console.log(popped);
+
+friends.shift();
+console.log(friends);
+
+// checks index of an element and if there even is such element in an array
+console.log(friends.indexOf('Steven'));
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+*/

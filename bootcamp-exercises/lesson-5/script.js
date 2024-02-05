b99ee3e869cfb6c6ee8aaf927bbec33eee049ba9
MadaraRumba <madara.rumba.rumba@gmail.com>
2024-02-05 23:30:51 +0200

    Lesson #5, tasks #1 and #2

--- bootcamp-exercises/lesson-5/script.js
+++ bootcamp-exercises/lesson-5/script.js
@@ -1,2 +1,36 @@
 'use strict';
+/*
+// Task #1
+// Declare and initialize array with four arbitrary whole numbers. Print out all of the numbers
+// in array. Then ask user to input whole number and assign it to the first element in array.
+// Then print out array again.
+
+let array = [5, 2, 66, 444];
+console.log(array);
+
+const input = prompt('Please enter a number');
+
+array = [5, input, 66, 444];
+console.log(array);
+
+*/
+
+
+// Task #2
+// Declare and initialize array with arbitrary whole numbers. Ask user to input whole number and check if your array contains this number. Print "Array contains <n>!" or "Array doesn't contain number <n>" if the array does or doesn't contain number.
+
+let numbers = [5, 7, 19, 1];
+
+let userInput = prompt('Please enter a whole number');
+
+function containsNumber(numbers, targetNumber){
+    return numbers.includes(targetNumber);
+}
+
+if (containsNumber(numbers, userInput)){
+    console.log(`Array contains Nr. ${userInput}'`);
+} else {
+    console.log(`Array doesn't contain Nr. ${userInput}`);
+}
+
 

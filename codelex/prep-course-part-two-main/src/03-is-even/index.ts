08c7b244eb61ceee9f898b0cdc40da5e5ee72204
MadaraRumba <madara.rumba.rumba@gmail.com>
2024-03-14 17:53:50 +0200

    Codelex folder #2

---
+++ codelex/prep-course-part-two-main/src/03-is-even/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Is Even
+ *
+ * Determine if given number is even
+ * without using any mathematic operators ( +, -, %, /, Math, ParseInt etc.)
+ *
+ * Examples:
+ * isEven(4) === true
+ * isEven(3) === false
+ */
+
+function isEven(n: number) {}
+
+export { isEven };
